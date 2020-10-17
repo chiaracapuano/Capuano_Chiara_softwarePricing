4f528050fbbb97f004479b2a973aa06c779ceff8
Chiara <ada.chiara.capuano@gmail.com>
2020-10-17 15:45:54 -0700

    back to elbow

--- main.py
+++ main.py
@@ -1,111 +1,112 @@
+
 import pandas as pd
 
 test_results = pd.read_csv("test_results.txt")
 test_results.drop(test_results.columns[0], axis=1, inplace=True)
 user_table = pd.read_csv("user_table.txt")
 
-
 df = test_results.merge(user_table, on ='user_id')
 
 
-#from pandas_profiling import ProfileReport
-#profile = ProfileReport(df, title='Pandas Profiling Report')
-#profile.to_file(output_file='profile.html')
-
 df.drop(columns = ['lat', 'long', 'user_id', 'country'], inplace=True)
+
+
 df['operative_system']=df.groupby('device').operative_system.transform(lambda x: x.fillna(x.mode()[0]))
 df = df.fillna(df.mode().iloc[0])
-print(df['timestamp'][0])
+
 
 df['timestamp'] =df['timestamp'].str.replace('60', '00')
 df['timestamp'] =pd.to_datetime(df['timestamp'], format='%Y-%m-%d %H:%M:%S', errors = 'coerce')
-print(df['timestamp'][0].hour)
-print(type(df['timestamp'][0]))
 
 
 df['hour'] = df['timestamp'].dt.hour
 df['month'] = df['timestamp'].dt.month
 df['weekday'] = df['timestamp'].dt.weekday
 df['year'] = df['timestamp'].dt.year
-#check for seasonality
-print(df['month'].value_counts())
+df['weeknum'] = df['timestamp'].dt.week
+
+
+print('Test months:',df['month'].value_counts())
 #check how historical the data is
-print(df['year'].value_counts())
-print('hour value counts',df['hour'].value_counts())
-print('weekday value counts',df['weekday'].value_counts())
+print('Test years:',df['year'].value_counts())
 
-df.drop(columns = ['month', 'year', 'timestamp'], inplace=True)
 
-#check trial users were actually displayed 59
+
+
+df.drop(columns = ['year', 'timestamp'], inplace=True)
+
+
 
 print(len(df[df['test']==1]))
 print(len(df[df['price']==59.0]))
 
-#remove inconsistent rows
+
+
 df = df[(df['test']==1) & (df['price']==59.0) | (df['test']==0) & (df['price']==39.0)]
 
-print(len(df))
+
+
+
 df.drop(columns = ['price'], inplace=True)
+df = df.dropna()
 
 
-#calculate the proportional revenue obtained by the test users vs non test users (google significance of ab test)
+from scipy.stats import chi2_contingency
+
+data_crosstab = pd.crosstab(df['test'],
+                            df['converted'],
+                            margins = False,
+                            normalize = 'index')
+chi2, p, dof, ex= chi2_contingency(data_crosstab)
+
+print(p)
+
+
+print('min month',df['month'].min())
+days = df[df['month']==3.0]
+print('min day in month 3',df['weekday'].min())
+
+#%% chi2 test in one week span
+
+min_week_num = df['weeknum'].min()
+first_week = df[(df['month']==3.0) & (df['weeknum']==min_week_num)]
+
+#%%
+
+data_crosstab = pd.crosstab(first_week['test'],
+                            first_week['converted'],
+                            margins = False,
+                            normalize = 'index')
+chi2, p, dof, ex= chi2_contingency(data_crosstab)
+
+print(p)
 
 
 
+df.drop(columns = ['month','weeknum','test'], inplace=True)
 
 
-
-
-
-
-
-
-
-#users clusters
 from sklearn import preprocessing
+df_enc = df
+to_encode = ['source', 'device', 'operative_system', 'city']
 le = preprocessing.LabelEncoder()
-df_enc = df.apply(le.fit_transform)
+
+df_enc[to_encode] = df_enc[to_encode].apply(le.fit_transform)
 
 
-#clustering with kmodes
+
+
+
+import scipy.cluster.hierarchy as shc
+from matplotlib import style
+style.use('seaborn-white')
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 7))
+plt.title("Customer Dendograms")
+dend = shc.dendrogram(shc.linkage(df_enc, method='ward'))
+plt.show()
+
 
 from kmodes.kmodes import KModes
 
-#cost=[]
-#for k in list(range(4,11)):
-#        print('Iteration number:', k)
-#        km = KModes(n_clusters=k, n_init = 1, verbose=1)
-#        km.fit(df_enc)
-#        cost.append(km.cost_)
-
-#import numpy as np
-#y = np.array([i for i in range(4,11,1)])
-#import matplotlib.pyplot as plt
-
-#plt.plot(y,cost)
-#plt.show()
-
-#choose k=5
-km = KModes(n_clusters=5, n_init = 1, verbose=1)
-clusters = km.fit_predict(df_enc)
-
-print('done with kmodes')
-
-df_ind_res = df_enc.reset_index()
-
-
-clustersDf = pd.DataFrame(clusters)
-clustersDf.columns = ['cluster_predicted']
-df_clusters = pd.concat([clustersDf, df_ind_res], axis = 1).reset_index()
-df_clusters = df_clusters.drop(['index', 'level_0'], axis = 1)
-
-print(df_clusters.head())
-for col in df_clusters.columns:
-    print(col)
-import matplotlib.pyplot as plt
-import seaborn as sns
-cols_clusters = ['source','device', 'operative_system','converted','city','hour','weekday']
-for col in df_enc.columns:
-    #plt.subplots(figsize = (15,5))
-    sns.countplot(x=df_clusters[col],order=df_clusters[col].value_counts().index,hue=df_clusters['cluster_predicted'])
-    plt.show()
